e72417f8bf74fdfe5ce855df662e56f7b8a7cc11
sonu 123 <m.p.ansuman10@gmail.com>
2024-05-11 00:36:05 +0530

    added template

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, room_details
 
 urlpatterns = [
     path("", home, name="home"),
+    path("room/<int:pk>/", room_details, name="roomDetails"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse("Hello World")
+    rooms = Room.objects.all()
+    context = {"rooms": rooms}
+    return render(request, "base/home.html", context)
+
+
+def room_details(request, pk):
+    try:
+        room = Room.objects.get(pk=int(pk))
+    except Exception as e:
+        return render(request, "base/room_details.html", {"error": f"{e}"})
+    return render(request, "base/room_details.html", {"room": room})
