096ff17e887c4b05f9ea48c7370dbfaa80181e64
sonu 123 <m.p.ansuman10@gmail.com>
2024-05-11 10:19:08 +0530

    updated model structure

--- base/models.py
+++ base/models.py
@@ -1,11 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
-class Room(models.Model):
+class Topic(models.Model):
     name = models.CharField(max_length=200)
-    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=200)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Message(models.Model):
+    room = models.ForeignKey(Room, null=True, on_delete=models.CASCADE, blank=True)
+    body = models.TextField(blank=True, null=True)
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.body[:10]
